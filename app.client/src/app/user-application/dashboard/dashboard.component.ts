6422dd1922f966884a56822d749fa20ac31bca7d
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-05-08 22:33:11 +0200

    Template of Favourites resources card in Dashboard

--- app.client/src/app/user-application/dashboard/dashboard.component.ts
+++ app.client/src/app/user-application/dashboard/dashboard.component.ts
@@ -1,40 +1,41 @@
 import { Component } from '@angular/core';
 import { FavouritsCardComponent } from '../favourites/ui/favourits-card/favourits-card.component';
 import { Resource, ResourceType } from '../favourites/model/resource';
-import { RoomsContainerComponent } from '../messages/ui/rooms-container/rooms-container.component';
+import { NotificationsFeedComponent } from '../notifications/ui/notifications-feed/notifications-feed.component';
+import { MessagesFeedComponent } from '../messages/ui/message-feed/message-feed.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FavouritsCardComponent, RoomsContainerComponent],
+  imports: [FavouritsCardComponent, NotificationsFeedComponent, MessagesFeedComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  // resources: Resource[] = [
-  //   {
-  //     id: '1',
-  //     name: 'Do your best',
-  //     type: ResourceType.TASK,
-  //     isFavourite: true,
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Raport 2024',
-  //     type: ResourceType.MATERIAL,
-  //     isFavourite: true,
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Schedule',
-  //     type: ResourceType.PATH,
-  //     isFavourite: true,
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Meet Up Gdańsk',
-  //     type: ResourceType.EVENT,
-  //     isFavourite: true,
-  //   },
-  // ];
+  resources: Resource[] = [
+    {
+      id: '1',
+      name: 'Do your best',
+      type: ResourceType.TASK,
+      isFavourite: true,
+    },
+    {
+      id: '2',
+      name: 'Raport 2024',
+      type: ResourceType.MATERIAL,
+      isFavourite: true,
+    },
+    {
+      id: '2',
+      name: 'Schedule',
+      type: ResourceType.PATH,
+      isFavourite: true,
+    },
+    {
+      id: '2',
+      name: 'Meet Up Gdańsk',
+      type: ResourceType.EVENT,
+      isFavourite: true,
+    },
+  ];
 }
