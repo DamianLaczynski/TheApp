2aa1322a57bfa7b1326f8e78c307679c8ec19c83
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-05-13 11:22:57 +0200

    Add ScheduleComponent and routing for it

--- app.client/src/app/app.config.ts
+++ app.client/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideRouter(routes, withComponentInputBinding())]
+  providers: [provideHttpClient(), DatePipe, provideRouter(routes, withComponentInputBinding())]
 };
